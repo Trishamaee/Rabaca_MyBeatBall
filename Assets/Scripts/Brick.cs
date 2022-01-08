8840c73472135ce61b45737372f40eb5b264bcd5
Trishamaee <rabacatrisha@gmail.com>
2022-01-08 23:47:32 +0800

    Added Brickhit Animation, GameManager, Panel

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,14 +6,22 @@ public class Brick : MonoBehaviour
 {
     public int hits = 2;
     public int points = 5;
+    public Vector3 rotator;
+    public Material hitMaterial;
+
+    Material _orgMaterial;
+    Renderer _renderer;
+
     void Start()
     {
-
+        //transform.Rotate(rotator * (transform.position.x + transform.position.y)* 0.1f);
+        _renderer = GetComponent<Renderer>();
+        _orgMaterial = _renderer.sharedMaterial;
     }
 
     void Update()
     {
-
+        //transform.Rotate(rotator * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -23,7 +31,18 @@ public class Brick : MonoBehaviour
         //score
         if (hits < -0)
         {
+            // Destroy() is a predefined variable that you can access to this game object to destory the object
             Destroy(gameObject);
         }
+
+        _renderer.sharedMaterial = hitMaterial;
+        Invoke("RestoreMaterials", 0.05f);
+
     }
+
+    void RestoreMaterials()
+    {
+        _renderer.sharedMaterial = _orgMaterial;
+    }
+
 }
